8cc3a41d2428c204d1f52900431f266fd0d66e01
William Pring <mrwilliampring@gmail.com>
2015-09-21 13:32:22 -0400

    Commiting the server side cpp files with all the new updated changes with the 3 functions working

--- 2nd year/Relation Databases/Assignment #1/ServerSide/ServerSide/ServerSide.cpp
+++ 2nd year/Relation Databases/Assignment #1/ServerSide/ServerSide/ServerSide.cpp
@@ -17,9 +17,9 @@
 
 #pragma comment(lib, "Ws2_32.lib")
 
-using namespace std; 
+using namespace std;
 
-DWORD WINAPI readAndWriteThread(LPVOID lpParam); 
+DWORD WINAPI readAndWriteThread(LPVOID lpParam);
 DWORD WINAPI closeServer(LPVOID lpParam);
 
 //The steps to create a server, create a socket, bind the socket, have the socket listen for connections, connect, read/write to client connected
@@ -30,15 +30,15 @@ char recvbuf[512] = "";
 SOCKET ClientSocket; //Temporary socket variable to accept connections from clients 
 SOCKET ListenSocket = INVALID_SOCKET;
 HANDLE newThread; //Handle for new thread creation 
-HANDLE closeSocketsThread; 
-HANDLE openMutex; 
+HANDLE closeSocketsThread;
+HANDLE openMutex;
 
 vector<int> socketPool;
 //vector<int>::iterator iter; 
 
 int main()
 {
-	WSADATA wsaData; 
+	WSADATA wsaData;
 	int iResult;
 
 	// Initialize Winsock
@@ -49,18 +49,18 @@ int main()
 		return 1;
 	}
 
-	struct addrinfo *result = NULL, *ptr = NULL, hints; 
+	struct addrinfo *result = NULL, *ptr = NULL, hints;
 
 	ZeroMemory(&hints, sizeof(hints));
-	hints.ai_family = AF_INET; 
-	hints.ai_socktype = SOCK_STREAM; 
+	hints.ai_family = AF_INET;
+	hints.ai_socktype = SOCK_STREAM;
 	hints.ai_protocol = IPPROTO_TCP;
-	hints.ai_flags = AI_PASSIVE; 
+	hints.ai_flags = AI_PASSIVE;
 
 	//So now you have to resolve the local address and port so that it can be used for the server 
 
 	iResult = getaddrinfo(NULL, DEFAULT_PORT, &hints, &result);
-	if (iResult != 0) 
+	if (iResult != 0)
 	{
 		printf("getaddrinfo failed: %d\n", iResult);
 		WSACleanup();
@@ -99,7 +99,7 @@ int main()
 	}
 
 	printf(">>Socket binded successfully\n\n");
-	
+
 	//When the bind function completes, the address information obtained from getaddrinfo is no longer required
 	//So just free the memory
 
@@ -137,7 +137,7 @@ int main()
 
 		// Accept a client socket
 		ClientSocket = accept(ListenSocket, NULL, NULL);
-		if (ClientSocket == INVALID_SOCKET) 
+		if (ClientSocket == INVALID_SOCKET)
 		{
 			printf("accept failed: %d\n", WSAGetLastError());
 			closesocket(ListenSocket);
@@ -145,15 +145,15 @@ int main()
 			return 1;
 		}
 
-		socketPool.push_back(ClientSocket); 
+		socketPool.push_back(ClientSocket);
 
-		newThread = CreateThread(NULL, 0, readAndWriteThread, (LPVOID)ClientSocket, 0, NULL); 
-		closeSocketsThread = CreateThread(NULL, 0, closeServer, (LPVOID)ClientSocket, 0, NULL); 
+		newThread = CreateThread(NULL, 0, readAndWriteThread, (LPVOID)ClientSocket, 0, NULL);
+		closeSocketsThread = CreateThread(NULL, 0, closeServer, (LPVOID)ClientSocket, 0, NULL);
 
 		printf("Client Number [%d] Connected\n", ClientSocket);
 	}
-	
-	return 0; 
+
+	return 0;
 }
 
 
@@ -163,83 +163,83 @@ DWORD WINAPI readAndWriteThread(LPVOID lpParam)
 {
 	printf("\n>>Thread created successfully\n");
 
-	int ClientSocket = (int)lpParam; 
+	int ClientSocket = (int)lpParam;
 
 	char* serverMenu = "Welcome to Database Server\n\n1-INSERT\n2-UPDATE\n3-FIND\n4-DISCONNECT\n";
-	char* defaultCase = "Character was unreadable\n\n"; 
+	char* defaultCase = "Character was unreadable\n\n";
 	char* busyServer = "Server is busy at the moment, please wait\n";
-	int	  numberChosen = 0; 
-	bool  leaveLoop = false; 
+	int	  numberChosen = 0;
+	bool  leaveLoop = false;
 	DWORD dwWaitResult;
 
 	dwWaitResult = WaitForSingleObject(openMutex, INFINITE);
 
 	switch (dwWaitResult)
 	{
-		case WAIT_OBJECT_0:	// TODO: Perform task
-		{
-			send(ClientSocket, serverMenu, (int)strlen(serverMenu), 0);
+	case WAIT_OBJECT_0:	// TODO: Perform task
+	{
+		send(ClientSocket, serverMenu, (int)strlen(serverMenu), 0);
 
-			do
+		do
+		{
+			recv(ClientSocket, recvbuf, recvbuflen, 0);
+			numberChosen = atoi(recvbuf);
+			memset(recvbuf, 0, strlen(recvbuf));
+
+			switch (numberChosen)
 			{
+			case 1:
+			{
+				//function for inserting into file
+				//send(ClientSocket, "You chose 1", (int)strlen("You chose 1"), 0);
 				recv(ClientSocket, recvbuf, recvbuflen, 0);
-				numberChosen = atoi(recvbuf);
-				memset(recvbuf, 0, strlen(recvbuf));
+				ofstream file;
+				file.open("database.txt", ios::app);
+				file << recvbuf;
+				file.close();
+				break;
+			}
+			case 2:
+			{
+				//function for updating the file 			
 
-				switch (numberChosen)
-				{
-					case 1:
-					{
-						//function for inserting into file
-						//send(ClientSocket, "You chose 1", (int)strlen("You chose 1"), 0);
-						recv(ClientSocket, recvbuf, recvbuflen, 0);
-						ofstream file;
-						file.open("database.txt", ios::app);
-						file << recvbuf;
-						file.close();
-						break;
-					}
-					case 2:
-					{
-						//function for updating the file 			
+				break;
+			}
+			case 3:
+			{
+				//function for finding a member in the file 
 
-						break;
-					}
-					case 3:
-					{
-						//function for finding a member in the file 
+				break;
+			}
+			case 4:
+			{
+				printf("Client Number [%d] Disconnected", ClientSocket);
+				send(ClientSocket, "Disconnected from server", (int)strlen("Disconnected from server"), 0);
+				leaveLoop = true;
+				break;
+			}
+			}
+		} while (leaveLoop != true);
 
-						break;
-					}
-					case 4:
-					{
-						printf("Client Number [%d] Disconnected", ClientSocket);
-						send(ClientSocket, "Disconnected from server", (int)strlen("Disconnected from server"), 0);
-						leaveLoop = true;
-						break;
-					}		
-				}
-			} while (leaveLoop != true);
-			
-			ReleaseMutex(openMutex);
-			break;
-		}
-		case WAIT_TIMEOUT:
-		{
-			send(ClientSocket, busyServer, (int)strlen(busyServer), 0);
-			printf("Thread %d: wait timed out\n", GetCurrentThreadId());
-			break;
-		}
-		default:
-		{
-			send(ClientSocket, busyServer, (int)strlen(busyServer), 0);
-			break; 
-		}
+		ReleaseMutex(openMutex);
+		break;
+	}
+	case WAIT_TIMEOUT:
+	{
+		send(ClientSocket, busyServer, (int)strlen(busyServer), 0);
+		printf("Thread %d: wait timed out\n", GetCurrentThreadId());
+		break;
+	}
+	default:
+	{
+		send(ClientSocket, busyServer, (int)strlen(busyServer), 0);
+		break;
+	}
 	}
 
 	printf("\nExited LOOP successfully\n");
 
-	return 0; 
+	return 0;
 }
 
 
@@ -278,4 +278,4 @@ DWORD WINAPI closeServer(LPVOID lpParam)
 
 //Client has to be able to disconnect gracefully
 
-//Server disconnects, everybody disconnects with a message, alarm.. 
+//Server disconnects, everybody disconnects with a message, alarm.. , everybody disconnects with a message, alarm.. 
