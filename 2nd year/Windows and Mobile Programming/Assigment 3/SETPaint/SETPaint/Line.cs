84109d39cb9e49f22d1afeabc0a25fa49cd05a36
William Pring <mrwilliampring@gmail.com>
2015-10-04 21:26:59 -0400

    Create the class for the paint program

---
+++ 2nd year/Windows and Mobile Programming/Assigment 3/SETPaint/SETPaint/Line.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SETPaint
+{
+    class Line : Shape
+    {
+        private Point myPenStartPoint;
+        private Point myPenEndPoint;
+        public Line()
+        {
+            myPenEndPoint = new Point(0, 0);
+            myPenStartPoint = new Point(0, 0);
+        } 
+        public Line(Point myPenStartPoint, Point myPenEndPoint, Pen pen)
+        {
+            this.myPenStartPoint = myPenStartPoint;
+            this.myPenEndPoint = myPenEndPoint;
+
+
+
+        }
+
+    }
+}
