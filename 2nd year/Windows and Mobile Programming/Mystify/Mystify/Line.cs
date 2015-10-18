d891c9df9d45702d008cadd9ac3e2073187810aa
William Pring <mrwilliampring@gmail.com>
2015-10-18 19:04:10 -0400

    having random lines...

--- 2nd year/Windows and Mobile Programming/Mystify/Mystify/Line.cs
+++ 2nd year/Windows and Mobile Programming/Mystify/Mystify/Line.cs
@@ -10,9 +10,11 @@ namespace Mystify
 {
     class Line
     {
+        Random newRnd = new Random();
         Pen myPen; 
         Point myLineStartPoint;
-        Point myLineEndPoint; 
+        Point myLineEndPoint;
+        private int rnd;
         bool status;
         public Line()
         {
@@ -23,7 +25,12 @@ namespace Mystify
             this.myLineStartPoint = myLineStartPoint;
             this.myLineEndPoint = myLineEndPoint;
             this.myPen = myPen;
-            status = true;  
+            status = true;
+            rnd = 0;
+        }
+        public void RandomNumber()
+        {
+            rnd = newRnd.Next(1, 10);
         }
         public void draw(object toDraw)
         {
@@ -32,8 +39,10 @@ namespace Mystify
             {
                 drawing.DrawLine(myPen, myLineStartPoint, myLineEndPoint);
                 Thread.Sleep(50);
-                myLineStartPoint.X = myLineStartPoint.X - 10;
-                myLineStartPoint.Y = myLineStartPoint.Y - 10;
+                myLineStartPoint.X = myLineStartPoint.X - rnd;
+                myLineStartPoint.Y = myLineStartPoint.Y - rnd;
+                myLineEndPoint.X = myLineEndPoint.X - rnd;
+                myLineEndPoint.Y = myLineEndPoint.Y - rnd;
                 drawing.DrawLine(myPen, myLineStartPoint, myLineEndPoint);
                 Thread.Sleep(50);
             }
