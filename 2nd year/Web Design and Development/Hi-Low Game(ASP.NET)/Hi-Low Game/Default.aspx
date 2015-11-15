f02054a9fe8f722b9fcfcfa077779879ad02de39
Williampring <mrwilliampring@gmail.com>
2015-11-14 18:22:06 -0800

    got the validation and input working and random numbers working on the game logic...

---
+++ 2nd year/Web Design and Development/Hi-Low Game(ASP.NET)/Hi-Low Game/Default.aspx
@@ -0,0 +1,30 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="Hi_Low_Game._Default" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Hi-Low Game in Asp.Net</title>
+</head>
+<body>
+    <form id="serverForm" runat="server">
+
+    <div>
+        <asp:Label ID="mainLabel" Text="Enter in your name: "  runat="server"/>
+        <asp:TextBox ID="gameTextbox" runat="server"/>
+        <asp:RequiredFieldValidator ID="validateEmptyTextbox" ValidationGroup="valForm" runat="server" ControlToValidate="gameTextbox" Text="* Required!" Display="Dynamic"></asp:RequiredFieldValidator>
+        <asp:RegularExpressionValidator ID="validateRegex" ValidationGroup="valForm" runat="server" ControlToValidate="gameTextbox" ValidationExpression="^[a-zA-Z''-'\s]{1,40}$" ErrorMessage="* Must only contain alphabets" Display="Dynamic"></asp:RegularExpressionValidator>
+        
+        <!-- 
+        <asp:Label ID="askForRandomNumber" Text="Enter in a number: " Visible="false" runat="server" />
+        <asp:TextBox CausesValidation="false" ID="hashNumberKey" Visible="false" runat="server" />
+        <asp:RequiredFieldValidator ID="requiredNumber" runat="server" ControlToValidate="hashNumberKey" ErrorMessage="Second Error Missing Message" Display="Dynamic"></asp:RequiredFieldValidator>
+        <asp:RegularExpressionValidator ID="numberValidatorTwo"  runat="server" ControlToValidate="hashNumberKey" ValidationExpression="\d+" ErrorMessage="Must enter a positive number." Display="Dynamic"></asp:RegularExpressionValidator>
+        <asp:RangeValidator ID="numberRangeValidator" runat="server" MinimumValue="2" MaximumValue="1000" Type="Integer" ControlToValidate="hashNumberKey" ErrorMessage="Number cannot be less than or equal to 1." Display="Dynamic"></asp:RangeValidator>
+        -->
+
+       <br /><br /><asp:Button onClick="submit" Text="Submit" runat="server" ValidationGroup="valForm" CausesValidation="true"/>
+    </div>
+    </form>
+</body>
+</html>
---
+++ 2nd year/Web Design and Development/Hi-Low Game(ASP.NET)/Hi-Low Game/Default.aspx.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Hi_Low_Game
+{
+    public partial class _Default : System.Web.UI.Page
+    {
+        string playerName;
+        string playerEnteredNumber; 
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(IsPostBack == false)
+            {
+                playerName = "";
+                ViewState["caseCount"] = 1;  
+            }
+        }
+
+        protected void submit(object sender, EventArgs e)
+        {
+            if(IsValid)
+            {
+                spotInGameQuestions(); 
+            }         
+        }
+     
+        public void spotInGameQuestions()
+        {         
+            if((int)ViewState["caseCount"] == 1) //Ask the user for a number
+            {
+                ViewState["caseCount"] = 2; 
+
+                ViewState["Name"] = gameTextbox.Text; 
+                playerName = ViewState["Name"].ToString(); 
+                
+                gameTextbox.Text = ""; //Clear the textbox                 
+                mainLabel.Text = "Welcome " + playerName + "!<br />Enter in a number: "; //New question for the label to display 
+
+                /** Change the regex of the validator **/
+                validateRegex.ValidationExpression = "\\d+";
+                validateRegex.ErrorMessage = "Must only be a number!";
+
+            }
+            else if ((int)ViewState["caseCount"] == 2) //The game starts, and the user will enter in answers, handled here 
+            {
+                int number = 0; 
+                ViewState["caseCount"] = 3;
+
+                ViewState["Number"] = gameTextbox.Text;
+                playerEnteredNumber = ViewState["Number"].ToString();
+
+                gameTextbox.Text = ""; //Clear the textbox                 
+                mainLabel.Text = ViewState["Name"].ToString() + ", enter in a number between 1 and " + playerEnteredNumber.ToString() + ":";
+               
+                //Generate the random number within a specified range 
+                number = Int32.Parse(ViewState["Number"].ToString());
+                randomNumberGenerator(number); 
+                mainLabel.Text += "<br />Easter Bunny says answer is: " + ViewState["gameAnswer"].ToString();
+            }
+            else if ((int)ViewState["caseCount"] == 3)
+            {
+                gameEngineLogic(); 
+            }
+        }
+
+        public void randomNumberGenerator(int number)
+        {
+            Random newNumber = new Random();
+            ViewState["gameAnswer"] = newNumber.Next(1, number);
+        }
+
+        public void gameEngineLogic()
+        {
+
+        }
+    }
+}
