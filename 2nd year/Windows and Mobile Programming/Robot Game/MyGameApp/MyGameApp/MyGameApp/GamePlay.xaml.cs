aab7a43d0af43509e2563648d4f6b51844d4cce9
WilliamPring <mrwilliampring@gmail.com>
2015-12-12 13:12:24 -0800

    commit

--- 2nd year/Windows and Mobile Programming/Robot Game/MyGameApp/MyGameApp/MyGameApp/GamePlay.xaml.cs
+++ 2nd year/Windows and Mobile Programming/Robot Game/MyGameApp/MyGameApp/MyGameApp/GamePlay.xaml.cs
@@ -191,7 +191,7 @@ namespace MyGameApp
 
                 lastRobotXPos = canvasXCell;
                 lastRobotYPos = canvasYCell;
-
+                //
                 myRobot.Height = canvasXCell;
                 myRobot.Width = canvasXCell;
                 lastIndexAvailable = 9;
