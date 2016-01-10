83f0becd48bb6d9f199ee8f896e98449b35cae3f
WilliamPring <mrwilliampring@gmail.com>
2016-01-10 16:41:57 -0500

    Commiting semi working project for windows only sends 1000 and 2000 bytes

---
+++ 2nd year/Industrial Application Development/TCP- UDP Send Receive Application/TCP/TCP/Header.h
@@ -0,0 +1,11 @@
+#include <stdio.h>       
+#include <stdlib.h>   
+#include <string.h>
+#include <conio.h>
+
+//WinSock
+#include<winsock2.h>
+#pragma comment(lib,"ws2_32.lib") //Winsock Library
+
+#pragma warning(disable : 4996)
+int myIsDigt(char *input);
