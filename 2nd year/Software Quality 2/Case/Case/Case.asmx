76768b99db65700fef47f5db4f7012f8ec0a75d3
WilliamPring <mrwilliampring@gmail.com>
2016-06-04 22:29:52 -0400

    Commiting project that is SOAP final project

---
+++ 2nd year/Software Quality 2/Case/Case/Case.asmx
@@ -0,0 +1,1 @@
+﻿<%@ WebService Language="C#" CodeBehind="Case.asmx.cs" Class="Case.Case" %>
---
+++ 2nd year/Software Quality 2/Case/Case/Case.asmx.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Web;
+using System.Web.Services;
+using System.Web.Services.Protocols;
+
+namespace Case
+{
+    /// <summary>
+    /// Summary description for Case
+    /// </summary>
+    [WebService(Namespace = "http://localhost/webservice/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+
+    public class Case : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string HelloWorld()
+        {
+            return "Hello World";
+        }
+
+        [WebMethod]
+        public string CaseConvert(string incoming, uint flag)
+        {
+            if (incoming == "")
+            {
+                throw new SoapException("Give me something to convert", new System.Xml.XmlQualifiedName("Empty String"));
+            }
+            else if (flag == 1)
+            {
+                incoming = incoming.ToUpper();
+            }
+            else if (flag == 2)
+            {
+                incoming = incoming.ToUpper();
+            }
+            else if (flag == 3)
+            {
+                incoming = Regex.Replace(incoming, @"\s+", "");
+            }
+            else
+            {
+                throw new SoapException("Invalid CaseConvert operation : operation = [1 for upper case, 2 for lower case, 3 for collapse space]", new System.Xml.XmlQualifiedName("Empty String"));
+            }
+            return incoming;
+        }
+    }
+}
