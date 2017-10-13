0e241003b790dd0fa0034113bdf3940d1c640a10
WilliamPring <mrwilliampring@gmail.com>
2017-10-12 20:05:12 -0400

    Commiting project with the client side view and processing as well

---
+++ 3rd year/Service Oriented Architecture/Assignment 3/Assignment 3/App_Code/TickerTape.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using NLog;
+using System.Web.Services.Protocols;
+using System.Text.RegularExpressions;
+
+/// <summary>
+/// Summary description for TickerTape
+/// </summary>
+[WebService(Namespace = "http://tempuri.org/")]
+[WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+// To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+// [System.Web.Script.Services.ScriptService]
+public class TickerTape : System.Web.Services.WebService
+{
+
+    private static Logger logger = LogManager.GetCurrentClassLogger();
+    ExceptionError eError = new ExceptionError();
+
+    delayedstockquote.DelayedStockQuote dsq = new delayedstockquote.DelayedStockQuote();
+    public struct QuoteInfo
+    {
+        public string Symbol;
+        public double LastPrice;
+        public string LastPriceDate;
+        public string LastPriceTime;
+    }
+
+
+
+
+    public TickerTape()
+    {
+
+        //Uncomment the following line if using designed components 
+        //InitializeComponent(); 
+    }
+
+    [WebMethod]
+    public QuoteInfo GetQuote(string tickerSymbol)
+    {
+        QuoteInfo qi = new QuoteInfo();
+
+        if ((Regex.IsMatch(tickerSymbol, @"[^A-Za-z0-9]+")) || (tickerSymbol== ""))
+        {
+            try
+            {
+                string errorString = "Invalid Ticker Format!";
+                string moreDetailError = "Ticker Symbol cannot have Symbols or cannot be empty";
+                eError.ThrowCustomSoapException(errorString, moreDetailError, Context.Request.Url.AbsoluteUri);
+            }
+            catch (SoapException se)
+            {
+                logger.Error(se);
+                throw se;
+            }
+        }
+
+        else
+        {
+
+            try
+            {
+                delayedstockquote.QuoteData qd = dsq.GetQuote(tickerSymbol, "0");
+                qi.Symbol = qd.StockSymbol;
+                qi.LastPrice = System.Convert.ToDouble(qd.LastTradeAmount);
+                qi.LastPriceDate = qd.LastTradeDateTime.Date.ToString();
+                qi.LastPriceTime = qd.LastTradeDateTime.TimeOfDay.ToString();
+            }
+            catch (SoapException se)
+            {
+                try
+                {
+                    // Input string .
+                    string errorString = "Input Ticker was not in a correct!";
+                    string moreDetailError = "Ticker Symbol was invalid!";
+                    eError.ThrowCustomSoapException(errorString, moreDetailError, Context.Request.Url.AbsoluteUri);
+                }
+                catch (SoapException sexp)
+                {
+                    logger.Error(sexp);
+                    throw sexp;
+                }
+            }
+
+        }
+
+
+
+
+
+        return qi;
+
+    }
+
+}
---
+++ 3rd year/Service Oriented Architecture/Assignment 3/Assignment 3/App_Code/VinniesLoanService.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Web.Services.Protocols;
+using NLog;
+
+/// <summary>
+/// Summary description for VinniesLoanService
+/// </summary>
+[WebService(Namespace = "http://tempuri.org/")]
+[WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+// To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+// [System.Web.Script.Services.ScriptService]
+public class VinniesLoanService : System.Web.Services.WebService
+{
+    private static Logger logger = LogManager.GetCurrentClassLogger();
+
+    ExceptionError eError = new ExceptionError();
+    public VinniesLoanService()
+    {
+
+        //Uncomment the following line if using designed components 
+        //InitializeComponent(); 
+    }
+
+    [WebMethod]
+    public float LoanPayment(float principle, float rate, int payments)
+    {
+        string errorString = "";
+        float monthlyPayment = 0;
+        if ((principle <= 0) || (rate<=0) || (payments <=0))
+        {
+            try
+            {
+                errorString = "Values should not be less then 0";
+                string moreDetailError = "Principle, Rate or Payment should not be less then 0";
+                eError.ThrowCustomSoapException(errorString, moreDetailError, Context.Request.Url.AbsoluteUri);
+            }
+            catch (SoapException se)
+            {
+                logger.Error(se);
+                throw se;
+            }   
+        }
+        else
+        {
+            try
+            {
+                monthlyPayment = (rate + rate / ((float)(Math.Pow((1 + rate), payments)) - 1)) * principle;
+                monthlyPayment = (float)Math.Round(monthlyPayment, 2);
+            }
+            catch(OverflowException ofe)
+            {
+                logger.Error(ofe);
+                errorString = "Number to large";
+                string moreDetailError = "Overflow Exception " + ofe.Message;
+                try
+                {
+                    eError.ThrowCustomSoapException(errorString, moreDetailError, Context.Request.Url.AbsoluteUri);
+                }
+                catch (SoapException se)
+                {
+                    logger.Error(se);
+                    throw se;
+                }
+            }
+            catch(Exception ex)
+            {
+                logger.Error(ex);
+                errorString = "Error server fault";
+                string moreDetailError = "Exception " + ex.Message;
+                try
+                {
+                    eError.ThrowCustomSoapException(errorString, moreDetailError, Context.Request.Url.AbsoluteUri);
+                }
+                catch (SoapException se)
+                {
+                    logger.Error(se);
+                    throw se;
+                }
+            }
+   
+        }
+        return monthlyPayment;
+    }
+
+}
